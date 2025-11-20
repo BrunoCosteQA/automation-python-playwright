ce72b02d1cffa90f8541883afa5dfba989131163
BrunoCosteQA <bruno_srk@hotmail.com>
2025-11-20 17:13:17 -0300

    feat: criacao de conta

--- tests/test_login.py
+++
@@ -1,9 +0,0 @@
-from pages.login_page import LoginPage
-
-
-def test_login_sucesso(page, screenshot_service, base_url):
-    login_page = LoginPage(page, screenshot_service)
-
-    login_page.abrir(base_url)
-    print('acessou area login')
-    login_page.realizar_login("thrashers.013@gmail.com", "senha_valida")
